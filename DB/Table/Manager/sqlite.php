beb10bf8d03b5a88f33388358863eafd977257f0
Mark Wiesemann <wiesemann@php.net>
2006-02-26 15:54:56 +0000

    - methods as a PEAR::DB workaround for ibase, pgsql, sqlite
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Table/trunk@208020 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/Table/Manager/sqlite.php
+++ DB/Table/Manager/sqlite.php
@@ -187,6 +187,201 @@ class DB_Table_Manager_sqlite {
         return $definition;
     }
 
+    
+    /**
+     * drop existing index
+     *
+     * @param string    $table         name of table that should be used in method
+     * @param string    $name         name of the index to be dropped
+     * @return mixed DB_OK on success, a PEAR error on failure
+     * @access public
+     */
+    function dropIndex($table, $name)
+    {
+        return $this->_db->query("DROP INDEX $name");
+    }
+
+
+    /**
+     * drop existing constraint
+     *
+     * @param string    $table         name of table that should be used in method
+     * @param string    $name         name of the constraint to be dropped
+     * @return mixed DB_OK on success, a PEAR error on failure
+     * @access public
+     */
+    function dropConstraint($table, $name)
+    {
+        if (strtolower($name) == 'primary') {
+            return DB_Table::throwError(
+                DB_TABLE_ERR_ALTER_INDEX_IMPOS,
+                '(primary)'
+            );
+        }
+
+        return $this->_db->query("DROP INDEX $name");
+    }
+
+
+    /**
+     * alter an existing table
+     *
+     * @param string $name         name of the table that is intended to be changed.
+     * @param array $changes     associative array that contains the details of each type
+     *                             of change that is intended to be performed. The types of
+     *                             changes that are currently supported are defined as follows:
+     *
+     *                             name
+     *
+     *                                New name for the table.
+     *
+     *                            add
+     *
+     *                                Associative array with the names of fields to be added as
+     *                                 indexes of the array. The value of each entry of the array
+     *                                 should be set to another associative array with the properties
+     *                                 of the fields to be added. The properties of the fields should
+     *                                 be the same as defined by the Metabase parser.
+     *
+     *
+     *                            remove
+     *
+     *                                Associative array with the names of fields to be removed as indexes
+     *                                 of the array. Currently the values assigned to each entry are ignored.
+     *                                 An empty array should be used for future compatibility.
+     *
+     *                            rename
+     *
+     *                                Associative array with the names of fields to be renamed as indexes
+     *                                 of the array. The value of each entry of the array should be set to
+     *                                 another associative array with the entry named name with the new
+     *                                 field name and the entry named Declaration that is expected to contain
+     *                                 the portion of the field declaration already in DBMS specific SQL code
+     *                                 as it is used in the CREATE TABLE statement.
+     *
+     *                            change
+     *
+     *                                Associative array with the names of the fields to be changed as indexes
+     *                                 of the array. Keep in mind that if it is intended to change either the
+     *                                 name of a field and any other properties, the change array entries
+     *                                 should have the new names of the fields as array indexes.
+     *
+     *                                The value of each entry of the array should be set to another associative
+     *                                 array with the properties of the fields to that are meant to be changed as
+     *                                 array entries. These entries should be assigned to the new values of the
+     *                                 respective properties. The properties of the fields should be the same
+     *                                 as defined by the Metabase parser.
+     *
+     *                            Example
+     *                                array(
+     *                                    'name' => 'userlist',
+     *                                    'add' => array(
+     *                                        'quota' => array(
+     *                                            'type' => 'integer',
+     *                                            'unsigned' => 1
+     *                                        )
+     *                                    ),
+     *                                    'remove' => array(
+     *                                        'file_limit' => array(),
+     *                                        'time_limit' => array()
+     *                                    ),
+     *                                    'change' => array(
+     *                                        'name' => array(
+     *                                            'length' => '20',
+     *                                            'definition' => array(
+     *                                                'type' => 'text',
+     *                                                'length' => 20,
+     *                                            ),
+     *                                        )
+     *                                    ),
+     *                                    'rename' => array(
+     *                                        'sex' => array(
+     *                                            'name' => 'gender',
+     *                                            'definition' => array(
+     *                                                'type' => 'text',
+     *                                                'length' => 1,
+     *                                                'default' => 'M',
+     *                                            ),
+     *                                        )
+     *                                    )
+     *                                )
+     *
+     * @param boolean $check     (ignored in DB_Table)
+     * @access public
+     *
+     * @return mixed DB_OK on success, a PEAR error on failure
+     */
+    function alterTable($name, $changes, $check)
+    {
+        $version = $this->_getServerVersion();
+        foreach ($changes as $change_name => $change) {
+            switch ($change_name) {
+            case 'add':
+                if ($version['major'] >= 3 && $version['minor'] >= 1) {
+                    break;
+                }
+            case 'name':
+                if ($version['major'] >= 3 && $version['minor'] >= 1) {
+                    break;
+                }
+            case 'remove':
+            case 'change':
+            case 'rename':
+            default:
+                return DB_Table::throwError(DB_TABLE_ERR_ALTER_TABLE_IMPOS);
+            }
+        }
+
+        $query = '';
+        if (array_key_exists('name', $changes)) {
+            $change_name = $this->_db->quoteIdentifier($changes['name']);
+            $query .= 'RENAME TO ' . $change_name;
+        }
+
+        if (array_key_exists('add', $changes)) {
+            foreach ($changes['add'] as $field_name => $field) {
+                if ($query) {
+                    $query.= ', ';
+                }
+                $query.= 'ADD COLUMN ' . $field_name . ' ' . $field;
+            }
+        }
+
+        if (!$query) {
+            return DB_OK;
+        }
+
+        $name = $this->_db->quoteIdentifier($name);
+        return $this->_db->query("ALTER TABLE $name $query");
+    }
+
+
+    /**
+     * return version information about the server
+     *
+     * @param string     $native  determines if the raw version string should be returned
+     * @return mixed array/string with version information or MDB2 error object
+     * @access private
+     */
+    function _getServerVersion($native = false)
+    {
+        if (!function_exists('sqlite_libversion')) {
+            return 0;  // error
+        }
+        $server_info = sqlite_libversion();
+        if (!$native) {
+            $tmp = explode('.', $server_info, 3);
+            $server_info = array(
+                'major' => isset($tmp[0]) ? $tmp[0] : null,
+                'minor' => isset($tmp[1]) ? $tmp[1] : null,
+                'patch' => isset($tmp[2]) ? $tmp[2] : null,
+                'extra' => null,
+                'native' => $server_info,
+            );
+        }
+        return $server_info;
+    }
+
 }
 
 ?>
