7a3aca1100d6f9e25e0ca2c7e8f364063d5fa617
David Morse <morse@php.net>
2007-06-09 04:02:15 +0000

    Modified to extend new DatabaseTest base class
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Table/trunk@237235 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/database/DeleteTest.php
+++ tests/database/DeleteTest.php
@@ -1,37 +1,10 @@
 <?php
-#require_once 'PHPUnit/TestCase.php';
-require_once 'PHPUnit2/Framework/TestCase.php';
-require_once 'DB/Table/Database.php';
+require_once 'DatabaseTest.php';
 
-#class DeleteTest extends PHPUnit_TestCase {
-class DeleteTest extends PHPUnit2_Framework_TestCase {
+class DeleteTest extends DatabaseTest 
+{
+    var $insert = false;
 
-    var $db = null;
-    var $name = null;
-    var $conn = null;
-    var $table = null;
-    var $primary_key = null;
-    var $ref = null;
-    var $ref_to = null;
-    var $link = null;
-    var $col = null;
-    var $foreign_col = null;
-
-    function setUp() 
-    {
-        // Create DB_Table_Database object $db1 with no DB/MDB2 connection
-        require 'db1/define.php';
-        $db1->addAllLinks();
-        $this->name    =  $db_name;
-        $this->conn    =& $conn;
-        $this->db      =& $db1;
-        $this->verbose =  $verbose;
-
-        foreach ($properties as $property_name) {
-            $this->$property_name = $$property_name;
-        }
-
-    }
 
     function testDeleteRef1()
     {
