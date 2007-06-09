a45efa5a56b286aafa28f282bc3c918e6e48fa68
David Morse <morse@php.net>
2007-06-09 04:03:29 +0000

    Now extends DatabaseTest
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Table/trunk@237236 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/database/GetTest.php
+++ tests/database/GetTest.php
@@ -1,47 +1,9 @@
 <?php
-#require_once 'PHPUnit/TestCase.php';
-require_once 'PHPUnit2/Framework/TestCase.php';
-require_once 'DB/Table/Database.php';
+require_once 'DatabaseTest.php';
 
+class GetTest extends DatabaseTest {
 
-#class GetTest extends PHPUnit_TestCase {
-class GetTest extends PHPUnit2_Framework_TestCase {
-
-    var $db = null;
-    var $name = null;
-    var $conn = null;
-    var $table = null;
-    var $primary_key = null;
-    var $table_subclass = null;
-    var $ref = null;
-    var $ref_to = null;
-    var $link = null;
-    var $col = null;
-    var $foreign_col = null;
-
-    function setUp() 
-    {
-        // Create DB_Table_Database object $db1 with no DB/MDB2 connection
-        require 'db1/define.php';
-        $db1->addAllLinks();
-        $this->name    =  $db_name;
-        $this->conn    =& $conn;
-        $this->db      =& $db1;
-        $this->verbose =  $verbose;
-
-        foreach ($properties as $property_name) {
-            $this->$property_name = $$property_name;
-        }
-
-    }
-
-    function tearDown() 
-    {
-        $tables = $this->db->_table;
-        foreach ($tables as $table) {
-            unset($table);
-        }
-    }
+    var $insert = false;
 
     function testGetTable1()
     {
