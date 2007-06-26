bfe576ebf4a436797fb86edace0f43913694fa18
David Morse <morse@php.net>
2007-06-26 04:33:37 +0000

    New files. Data files for regression testing.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Table/trunk@238627 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/database/SelectTest/testSelect1.php
@@ -0,0 +1,800 @@
+<?php
+$this->expect = array();
+$this->expect['Person'] =
+array (
+  0 => 
+  array (
+    'PersonID' => '1',
+    'FirstName' => 'ANTOINETTE',
+    'MiddleName' => 'MARIE',
+    'LastName' => 'ARLAND',
+    'NameSuffix' => NULL,
+  ),
+  1 => 
+  array (
+    'PersonID' => '2',
+    'FirstName' => 'MACHEL',
+    'MiddleName' => 'KAY',
+    'LastName' => 'WHITMORE',
+    'NameSuffix' => NULL,
+  ),
+  2 => 
+  array (
+    'PersonID' => '3',
+    'FirstName' => 'ANTHONY',
+    'MiddleName' => 'DAVID',
+    'LastName' => 'WESTVEER',
+    'NameSuffix' => NULL,
+  ),
+  3 => 
+  array (
+    'PersonID' => '4',
+    'FirstName' => 'JODY',
+    'MiddleName' => 'JEAN',
+    'LastName' => 'LARSON',
+    'NameSuffix' => NULL,
+  ),
+  4 => 
+  array (
+    'PersonID' => '5',
+    'FirstName' => 'JOSEPH',
+    'MiddleName' => 'FRANCISCO',
+    'LastName' => 'CRUZ',
+    'NameSuffix' => NULL,
+  ),
+  5 => 
+  array (
+    'PersonID' => '6',
+    'FirstName' => 'CHRISTINE',
+    'MiddleName' => 'MICHELLE',
+    'LastName' => 'KAPSNER',
+    'NameSuffix' => NULL,
+  ),
+  6 => 
+  array (
+    'PersonID' => '7',
+    'FirstName' => 'JEVENY',
+    'MiddleName' => NULL,
+    'LastName' => 'HAMMER',
+    'NameSuffix' => NULL,
+  ),
+  7 => 
+  array (
+    'PersonID' => '8',
+    'FirstName' => 'JOHNNIE',
+    'MiddleName' => 'ESPINOZA',
+    'LastName' => 'MARTINEZ',
+    'NameSuffix' => NULL,
+  ),
+  8 => 
+  array (
+    'PersonID' => '9',
+    'FirstName' => 'STEWART',
+    'MiddleName' => 'CHARLES',
+    'LastName' => 'SCHENCK',
+    'NameSuffix' => NULL,
+  ),
+  9 => 
+  array (
+    'PersonID' => '10',
+    'FirstName' => 'ANN',
+    'MiddleName' => NULL,
+    'LastName' => 'GOLDBERG',
+    'NameSuffix' => NULL,
+  ),
+  10 => 
+  array (
+    'PersonID' => '11',
+    'FirstName' => 'CHAD',
+    'MiddleName' => 'GERALD',
+    'LastName' => 'FLIES',
+    'NameSuffix' => NULL,
+  ),
+  11 => 
+  array (
+    'PersonID' => '12',
+    'FirstName' => 'STEPHANIE',
+    'MiddleName' => 'ANN',
+    'LastName' => 'WRIGHT',
+    'NameSuffix' => NULL,
+  ),
+  12 => 
+  array (
+    'PersonID' => '13',
+    'FirstName' => 'JEAN',
+    'MiddleName' => 'ADELE',
+    'LastName' => 'KALIL',
+    'NameSuffix' => NULL,
+  ),
+  13 => 
+  array (
+    'PersonID' => '14',
+    'FirstName' => 'JEFFREY',
+    'MiddleName' => 'DAVID',
+    'LastName' => 'KUTSCHEID',
+    'NameSuffix' => NULL,
+  ),
+  14 => 
+  array (
+    'PersonID' => '15',
+    'FirstName' => 'JENNIFER',
+    'MiddleName' => 'JEAN',
+    'LastName' => 'MACKENTHUN',
+    'NameSuffix' => NULL,
+  ),
+  15 => 
+  array (
+    'PersonID' => '16',
+    'FirstName' => 'MICHAEL',
+    'MiddleName' => 'JOHN',
+    'LastName' => 'SELANDER',
+    'NameSuffix' => NULL,
+  ),
+  16 => 
+  array (
+    'PersonID' => '17',
+    'FirstName' => 'SARAH',
+    'MiddleName' => 'JANE',
+    'LastName' => 'TENNES',
+    'NameSuffix' => NULL,
+  ),
+  17 => 
+  array (
+    'PersonID' => '18',
+    'FirstName' => 'ANDREW',
+    'MiddleName' => 'PAUL',
+    'LastName' => 'WUKMIR',
+    'NameSuffix' => NULL,
+  ),
+  18 => 
+  array (
+    'PersonID' => '19',
+    'FirstName' => 'SHERRY',
+    'MiddleName' => 'MARIE',
+    'LastName' => 'CHAPMAN',
+    'NameSuffix' => NULL,
+  ),
+  19 => 
+  array (
+    'PersonID' => '20',
+    'FirstName' => 'JEFFREY',
+    'MiddleName' => 'LEE',
+    'LastName' => 'WHITMORE',
+    'NameSuffix' => NULL,
+  ),
+);
+$this->expect['Address'] =
+array (
+  0 => 
+  array (
+    'AddressID' => '1',
+    'Building' => '13188',
+    'Street' => 'CARDINAL CREEK RD',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  1 => 
+  array (
+    'AddressID' => '2',
+    'Building' => '5037',
+    'Street' => 'NORMAN DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+  2 => 
+  array (
+    'AddressID' => '3',
+    'Building' => '14409',
+    'Street' => 'GRENIER RD',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+  3 => 
+  array (
+    'AddressID' => '4',
+    'Building' => '13411',
+    'Street' => 'MAYWOOD CURV',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+  4 => 
+  array (
+    'AddressID' => '5',
+    'Building' => '14408',
+    'Street' => 'WOODHILL CIR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+  5 => 
+  array (
+    'AddressID' => '6',
+    'Building' => '6135',
+    'Street' => 'CHASEWOOD PKWY',
+    'UnitType' => 'APT',
+    'Unit' => '204',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55343',
+  ),
+  6 => 
+  array (
+    'AddressID' => '7',
+    'Building' => '16335',
+    'Street' => 'TEMPLE DR S',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+  7 => 
+  array (
+    'AddressID' => '8',
+    'Building' => '10995',
+    'Street' => 'MELS WAY',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55344',
+  ),
+  8 => 
+  array (
+    'AddressID' => '9',
+    'Building' => '17168',
+    'Street' => 'PADONS DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  9 => 
+  array (
+    'AddressID' => '10',
+    'Building' => '7610',
+    'Street' => 'SMETANA LN',
+    'UnitType' => 'UNIT',
+    'Unit' => '103',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55344',
+  ),
+  10 => 
+  array (
+    'AddressID' => '11',
+    'Building' => '16379',
+    'Street' => 'MANOR RD S',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  11 => 
+  array (
+    'AddressID' => '12',
+    'Building' => '17439',
+    'Street' => 'RUSTIC HILLS DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  12 => 
+  array (
+    'AddressID' => '13',
+    'Building' => '5403',
+    'Street' => 'POMPANO DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55343',
+  ),
+  13 => 
+  array (
+    'AddressID' => '14',
+    'Building' => '6415',
+    'Street' => 'GRAND VIEW DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  14 => 
+  array (
+    'AddressID' => '15',
+    'Building' => '6425',
+    'Street' => 'CITY WEST PKWY',
+    'UnitType' => 'APT',
+    'Unit' => '3314',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55344',
+  ),
+  15 => 
+  array (
+    'AddressID' => '16',
+    'Building' => '5978',
+    'Street' => 'CHASEWOOD PKWY',
+    'UnitType' => 'APT',
+    'Unit' => '003',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55343',
+  ),
+  16 => 
+  array (
+    'AddressID' => '17',
+    'Building' => '13700',
+    'Street' => 'VALLEY VIEW RD',
+    'UnitType' => 'APT',
+    'Unit' => '107',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55344',
+  ),
+  17 => 
+  array (
+    'AddressID' => '18',
+    'Building' => '17460',
+    'Street' => 'RUSTIC HILLS DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55346',
+  ),
+  18 => 
+  array (
+    'AddressID' => '19',
+    'Building' => '10721',
+    'Street' => 'SMETANA RD',
+    'UnitType' => 'APT',
+    'Unit' => '103',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55343',
+  ),
+  19 => 
+  array (
+    'AddressID' => '20',
+    'Building' => '5037',
+    'Street' => 'NORMAN DR',
+    'UnitType' => NULL,
+    'Unit' => NULL,
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'ZipCode' => '55345',
+  ),
+);
+$this->expect['Phone'] =
+array (
+  0 => 
+  array (
+    'PhoneID' => '1',
+    'PhoneNumber' => '9528940424',
+  ),
+  1 => 
+  array (
+    'PhoneID' => '2',
+    'PhoneNumber' => '9529745279',
+  ),
+  2 => 
+  array (
+    'PhoneID' => '3',
+    'PhoneNumber' => '9529499236',
+  ),
+  3 => 
+  array (
+    'PhoneID' => '4',
+    'PhoneNumber' => '6513346195',
+  ),
+  4 => 
+  array (
+    'PhoneID' => '5',
+    'PhoneNumber' => '9529450055',
+  ),
+  5 => 
+  array (
+    'PhoneID' => '6',
+    'PhoneNumber' => '9529310051',
+  ),
+  6 => 
+  array (
+    'PhoneID' => '7',
+    'PhoneNumber' => '9529300438',
+  ),
+  7 => 
+  array (
+    'PhoneID' => '8',
+    'PhoneNumber' => '9529349246',
+  ),
+  8 => 
+  array (
+    'PhoneID' => '9',
+    'PhoneNumber' => '9528942288',
+  ),
+  9 => 
+  array (
+    'PhoneID' => '10',
+    'PhoneNumber' => '9529496790',
+  ),
+  10 => 
+  array (
+    'PhoneID' => '11',
+    'PhoneNumber' => '9529385751',
+  ),
+  11 => 
+  array (
+    'PhoneID' => '12',
+    'PhoneNumber' => '9529755977',
+  ),
+  12 => 
+  array (
+    'PhoneID' => '13',
+    'PhoneNumber' => '9529743785',
+  ),
+  13 => 
+  array (
+    'PhoneID' => '14',
+    'PhoneNumber' => '9529331534',
+  ),
+  14 => 
+  array (
+    'PhoneID' => '15',
+    'PhoneNumber' => '9529492390',
+  ),
+  15 => 
+  array (
+    'PhoneID' => '16',
+    'PhoneNumber' => '9529344367',
+  ),
+  16 => 
+  array (
+    'PhoneID' => '17',
+    'PhoneNumber' => '9529350387',
+  ),
+  17 => 
+  array (
+    'PhoneID' => '18',
+    'PhoneNumber' => '9529745079',
+  ),
+);
+$this->expect['PersonAddress'] =
+array (
+  0 => 
+  array (
+    'PersonID2' => '1',
+    'AddressID' => '1',
+  ),
+  1 => 
+  array (
+    'PersonID2' => '2',
+    'AddressID' => '2',
+  ),
+  2 => 
+  array (
+    'PersonID2' => '3',
+    'AddressID' => '3',
+  ),
+  3 => 
+  array (
+    'PersonID2' => '4',
+    'AddressID' => '4',
+  ),
+  4 => 
+  array (
+    'PersonID2' => '5',
+    'AddressID' => '5',
+  ),
+  5 => 
+  array (
+    'PersonID2' => '6',
+    'AddressID' => '6',
+  ),
+  6 => 
+  array (
+    'PersonID2' => '7',
+    'AddressID' => '7',
+  ),
+  7 => 
+  array (
+    'PersonID2' => '8',
+    'AddressID' => '8',
+  ),
+  8 => 
+  array (
+    'PersonID2' => '9',
+    'AddressID' => '9',
+  ),
+  9 => 
+  array (
+    'PersonID2' => '10',
+    'AddressID' => '10',
+  ),
+  10 => 
+  array (
+    'PersonID2' => '11',
+    'AddressID' => '11',
+  ),
+  11 => 
+  array (
+    'PersonID2' => '12',
+    'AddressID' => '12',
+  ),
+  12 => 
+  array (
+    'PersonID2' => '13',
+    'AddressID' => '13',
+  ),
+  13 => 
+  array (
+    'PersonID2' => '14',
+    'AddressID' => '14',
+  ),
+  14 => 
+  array (
+    'PersonID2' => '15',
+    'AddressID' => '15',
+  ),
+  15 => 
+  array (
+    'PersonID2' => '16',
+    'AddressID' => '16',
+  ),
+  16 => 
+  array (
+    'PersonID2' => '17',
+    'AddressID' => '17',
+  ),
+  17 => 
+  array (
+    'PersonID2' => '18',
+    'AddressID' => '18',
+  ),
+  18 => 
+  array (
+    'PersonID2' => '19',
+    'AddressID' => '19',
+  ),
+  19 => 
+  array (
+    'PersonID2' => '20',
+    'AddressID' => '20',
+  ),
+);
+$this->expect['PersonPhone'] =
+array (
+  0 => 
+  array (
+    'PersonID' => '1',
+    'PhoneID' => '1',
+  ),
+  1 => 
+  array (
+    'PersonID' => '2',
+    'PhoneID' => '2',
+  ),
+  2 => 
+  array (
+    'PersonID' => '3',
+    'PhoneID' => '3',
+  ),
+  3 => 
+  array (
+    'PersonID' => '4',
+    'PhoneID' => '4',
+  ),
+  4 => 
+  array (
+    'PersonID' => '5',
+    'PhoneID' => '5',
+  ),
+  5 => 
+  array (
+    'PersonID' => '6',
+    'PhoneID' => '6',
+  ),
+  6 => 
+  array (
+    'PersonID' => '7',
+    'PhoneID' => '7',
+  ),
+  7 => 
+  array (
+    'PersonID' => '9',
+    'PhoneID' => '8',
+  ),
+  8 => 
+  array (
+    'PersonID' => '10',
+    'PhoneID' => '9',
+  ),
+  9 => 
+  array (
+    'PersonID' => '11',
+    'PhoneID' => '10',
+  ),
+  10 => 
+  array (
+    'PersonID' => '13',
+    'PhoneID' => '11',
+  ),
+  11 => 
+  array (
+    'PersonID' => '14',
+    'PhoneID' => '12',
+  ),
+  12 => 
+  array (
+    'PersonID' => '15',
+    'PhoneID' => '13',
+  ),
+  13 => 
+  array (
+    'PersonID' => '16',
+    'PhoneID' => '14',
+  ),
+  14 => 
+  array (
+    'PersonID' => '17',
+    'PhoneID' => '15',
+  ),
+  15 => 
+  array (
+    'PersonID' => '18',
+    'PhoneID' => '16',
+  ),
+  16 => 
+  array (
+    'PersonID' => '19',
+    'PhoneID' => '17',
+  ),
+  17 => 
+  array (
+    'PersonID' => '20',
+    'PhoneID' => '18',
+  ),
+);
+$this->expect['Street'] =
+array (
+  0 => 
+  array (
+    'Street' => 'CARDINAL CREEK RD',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  1 => 
+  array (
+    'Street' => 'NORMAN DR',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  2 => 
+  array (
+    'Street' => 'GRENIER RD',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  3 => 
+  array (
+    'Street' => 'MAYWOOD CURV',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  4 => 
+  array (
+    'Street' => 'WOODHILL CIR',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  5 => 
+  array (
+    'Street' => 'CHASEWOOD PKWY',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  6 => 
+  array (
+    'Street' => 'TEMPLE DR S',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  7 => 
+  array (
+    'Street' => 'MELS WAY',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  8 => 
+  array (
+    'Street' => 'PADONS DR',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  9 => 
+  array (
+    'Street' => 'SMETANA LN',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  10 => 
+  array (
+    'Street' => 'MANOR RD S',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  11 => 
+  array (
+    'Street' => 'RUSTIC HILLS DR',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  12 => 
+  array (
+    'Street' => 'POMPANO DR',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  13 => 
+  array (
+    'Street' => 'GRAND VIEW DR',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  14 => 
+  array (
+    'Street' => 'CITY WEST PKWY',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  15 => 
+  array (
+    'Street' => 'VALLEY VIEW RD',
+    'City' => 'EDEN PRAIRIE',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+  16 => 
+  array (
+    'Street' => 'SMETANA RD',
+    'City' => 'MINNETONKA',
+    'StateAbb' => 'MN',
+    'Sunny' => NULL,
+  ),
+);
+?>
---
+++ tests/database/SelectTest/testSelectResult1.php
@@ -0,0 +1,106 @@
+<?php
+$this->expect = array();
+$this->expect['Result'] =
+array (
+  0 => 
+  array (
+    0 => 'CHAPMAN',
+    1 => 'SHERRY',
+    2 => '9529350387',
+    3 => '10721',
+    4 => 'SMETANA RD',
+    5 => 'MINNETONKA',
+    6 => '55343',
+  ),
+  1 => 
+  array (
+    0 => 'CRUZ',
+    1 => 'JOSEPH',
+    2 => '9529450055',
+    3 => '14408',
+    4 => 'WOODHILL CIR',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+  2 => 
+  array (
+    0 => 'HAMMER',
+    1 => 'JEVENY',
+    2 => '9529300438',
+    3 => '16335',
+    4 => 'TEMPLE DR S',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+  3 => 
+  array (
+    0 => 'KALIL',
+    1 => 'JEAN',
+    2 => '9529385751',
+    3 => '5403',
+    4 => 'POMPANO DR',
+    5 => 'MINNETONKA',
+    6 => '55343',
+  ),
+  4 => 
+  array (
+    0 => 'KAPSNER',
+    1 => 'CHRISTINE',
+    2 => '9529310051',
+    3 => '6135',
+    4 => 'CHASEWOOD PKWY',
+    5 => 'MINNETONKA',
+    6 => '55343',
+  ),
+  5 => 
+  array (
+    0 => 'LARSON',
+    1 => 'JODY',
+    2 => '6513346195',
+    3 => '13411',
+    4 => 'MAYWOOD CURV',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+  6 => 
+  array (
+    0 => 'SELANDER',
+    1 => 'MICHAEL',
+    2 => '9529331534',
+    3 => '5978',
+    4 => 'CHASEWOOD PKWY',
+    5 => 'MINNETONKA',
+    6 => '55343',
+  ),
+  7 => 
+  array (
+    0 => 'WESTVEER',
+    1 => 'ANTHONY',
+    2 => '9529499236',
+    3 => '14409',
+    4 => 'GRENIER RD',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+  8 => 
+  array (
+    0 => 'WHITMORE',
+    1 => 'JEFFREY',
+    2 => '9529745079',
+    3 => '5037',
+    4 => 'NORMAN DR',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+  9 => 
+  array (
+    0 => 'WHITMORE',
+    1 => 'MACHEL',
+    2 => '9529745279',
+    3 => '5037',
+    4 => 'NORMAN DR',
+    5 => 'MINNETONKA',
+    6 => '55345',
+  ),
+);
+?>
