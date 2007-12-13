3e4b693f82c6d8e3b614b135576366962079bdfa
Mark Wiesemann <wiesemann@php.net>
2007-12-13 16:52:15 +0000

    - change license to New BSD license   (DB_Table_Date class was marked as licensed   under PHP License 2.02 but Date package is   nowadays licensed unter the New BSD license) - adjust header comment blocks in all files - add more clear pointers to other packages   (DB_DataObject and MDB2) - add authors of the other packages - update changelog - package2.xml is now valid and can be used   for installing from CVS (release date will likely   change)
    git-svn-id: http://svn.php.net/repository/pear/packages/DB_Table/trunk@248151 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/Table/Manager.php
+++ DB/Table/Manager.php
@@ -1,24 +1,55 @@
 <?php
 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
 /**
-* 
-* Creates, checks or alters tables from DB_Table definitions.
-* 
-* DB_Table_Manager provides database automated table creation
-* facilities.
-* 
-* @category Database
-* 
-* @package DB_Table
-*
-* @author Paul M. Jones <pmjones@php.net>
-* @author Mark Wiesemann <wiesemann@php.net>
-* 
-* @license http://www.gnu.org/copyleft/lesser.html LGPL
-* 
-* @version $Id$
-*
-*/
+ * Creates, checks or alters tables from DB_Table definitions.
+ * 
+ * DB_Table_Manager provides database automated table creation
+ * facilities.
+ * 
+ * PHP versions 4 and 5
+ *
+ * LICENSE:
+ * 
+ * Copyright (c) 1997-2007, Paul M. Jones <pmjones@php.net>
+ *                          David C. Morse <morse@php.net>
+ *                          Mark Wiesemann <wiesemann@php.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the 
+ *      documentation and/or other materials provided with the distribution.
+ *    * The names of the authors may not be used to endorse or promote products 
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category Database
+ * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
+ * @author   David C. Morse <morse@php.net>
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @license  http://opensource.org/licenses/bsd-license.php New BSD License
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/DB_Table
+ */
 
 require_once 'DB/Table.php';
 
@@ -169,18 +200,19 @@ $GLOBALS['_DB_TABLE']['mdb2_type'] = array(
 );
 
 /**
-* 
-* Creates, checks or alters tables from DB_Table definitions.
-* 
-* DB_Table_Manager provides database automated table creation
-* facilities.
-* 
-* @category Database
-* @package DB_Table
-* @author Paul M. Jones <pmjones@php.net>
-* @author Mark Wiesemann <wiesemann@php.net>
-*
-*/
+ * Creates, checks or alters tables from DB_Table definitions.
+ * 
+ * DB_Table_Manager provides database automated table creation
+ * facilities.
+ * 
+ * @category Database
+ * @package  DB_Table
+ * @author   Paul M. Jones <pmjones@php.net>
+ * @author   David C. Morse <morse@php.net>
+ * @author   Mark Wiesemann <wiesemann@php.net>
+ * @version  Release: @package_version@
+ * @link     http://pear.php.net/package/DB_Table
+ */
 class DB_Table_Manager {
 
 
